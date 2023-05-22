2c4bec0ab307979701110f7448c52873fc299152
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2023-05-22 11:58:31 -0300

    InicioPagAppVeu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
-
+  import { livros } from "@/_data/livros"
 </script>
 
 <template>
   <h1>MINDE</h1>
+  <div class="livro">
+  <div class="card-livros" v-for="livro in livros" :key="livro.id">
+    <img src="livro.cover" alt="" class="livro-capa">
+    <p class="livro-titulo">{{ livro.title }}</p>
+    <p class="livro-autor">{{ livro.author }}</p>
+    <p class="livro-preco">{{ livro.price }}</p>
+  </div>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/_data/livros.js
@@ -0,0 +1,37 @@
+export const livros = [
+    {
+      id: 1,
+      title: "O poderoso chefão",
+      author: "Gira",
+      price: 49.9,
+      cover: "https://preview.redd.it/izwow92v591b1.jpg?width=640&crop=smart&auto=webp&v=enabled&s=9367ec218492edf88207fd0354e57800e7afb5fd",
+    },
+    {
+      id: 2,
+      title: "O chefão",
+      author: "Gira",
+      price: 39.9,
+      cover: "https://preview.redd.it/izwow92v591b1.jpg?width=640&crop=smart&auto=webp&v=enabled&s=9367ec218492edf88207fd0354e57800e7afb5fd",
+    },
+    {
+      id: 3,
+      title: "O poderoso",
+      author: "Gira",
+      price: 39.9,
+      cover: "https://preview.redd.it/izwow92v591b1.jpg?width=640&crop=smart&auto=webp&v=enabled&s=9367ec218492edf88207fd0354e57800e7afb5fd",
+    },
+    {
+      id: 4,
+      title: "chefão",
+      author: "Gira",
+      price: 29.9,
+      cover: "https://preview.redd.it/izwow92v591b1.jpg?width=640&crop=smart&auto=webp&v=enabled&s=9367ec218492edf88207fd0354e57800e7afb5fd",
+    },
+    {
+      id: 5,
+      title: "poderoso",
+      author: "Gira",
+      price: 29.9,
+      cover: "https://preview.redd.it/izwow92v591b1.jpg?width=640&crop=smart&auto=webp&v=enabled&s=9367ec218492edf88207fd0354e57800e7afb5fd",
+    }
+  ]
